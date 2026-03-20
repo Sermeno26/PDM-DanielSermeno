e86607c498baf2f09b9e36140e647514985752e5
Sermex2614 <daniel.sermeno@unplug.studio>
2026-03-20 11:23:10 -0600

    Agregar ejercicio 1

---
+++ app/src/test/java/com/example/pdm_labo_1/Ejercicio_1_Computadora.kt
@@ -0,0 +1,68 @@
+package com.example.pdm_labo_1
+
+import org.junit.Test
+data class Programa(
+    val nombre: String
+)
+
+class Computadora(
+    var RAM: Int,
+    var memoria: Int,
+    var SisOperativo: String,
+    var programasInstalados: List<Programa>
+) {
+
+    fun encender() {
+        println("Computadora encendida")
+    }
+
+    fun apagar() {
+        println("La computadora se ha apagado")
+    }
+
+    fun cambiarRAM(nuevaRAM: Int) {
+        RAM = nuevaRAM
+        println("RAM actualizada a $RAM GB")
+    }
+
+    fun cambiarSisOperativo(nuevoSO: String) {
+        SisOperativo = nuevoSO
+        println("Nuevo SO: $SisOperativo")
+    }
+
+    fun ProgramasAnio(anio: String): List<Programa> {
+        return programasInstalados.filter { it.nombre.contains(anio) }
+    }
+}
+
+class ComputadoraTest {
+
+    @Test
+    fun probarComputadora() {
+
+        val programas = listOf(
+            Programa("Notion 2026"),
+            Programa("Facebook 2024"),
+            Programa("WhatsApp 2026")
+        )
+
+        val pc = Computadora(
+            8,
+            256,
+            "Windows 10",
+            programas
+        )
+
+        pc.encender()
+        pc.cambiarRAM(16)
+
+        val resultado = pc.ProgramasAnio("2026")
+
+        println("Programas del 2026:")
+        resultado.forEach {
+            println(it.nombre)
+        }
+
+        pc.apagar()
+    }
+}
--- app/src/test/java/com/example/pdm_labo_1/Ejercicios.kt
+++
@@ -1,5 +0,0 @@
-package com.example.pdm_labo_1
-
-class Ejercicios {
-
-}
