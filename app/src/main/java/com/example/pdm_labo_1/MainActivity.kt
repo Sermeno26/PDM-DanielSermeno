83c68e5c79870cda685892991fc09144a00d1e5a
Sermex2614 <daniel.sermeno@unplug.studio>
2026-03-20 15:26:11 -0600

    Capturas de pantalla

--- app/src/main/java/com/example/pdm_labo_1/MainActivity.kt
+++ app/src/main/java/com/example/pdm_labo_1/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     PDM_Labo_1Theme {
-        Greeting("Android")
+        Greeting("Daniel")
     }
 }
